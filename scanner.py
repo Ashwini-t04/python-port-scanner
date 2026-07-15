8514de2c270a354e78a25c1d997ce06420a9cf5e
ashwini <nayakashwini004@gmail.com>
2026-07-15 13:57:21 +0530

    Version 7.0 - Added Command-Line Interface

--- scanner.py
+++ scanner.py
@@ -3,6 +3,7 @@ import sys
 import csv
 import json
 import threading
+import argparse
 from datetime import datetime
 services = {
     20: "FTP Data",
@@ -19,17 +20,33 @@ services = {
     3389: "RDP"
 }
 lock = threading.Lock()
+def grab_banner(sock, port):
+    try:
+        if port == 80:
+            sock.send(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
+
+        banner = sock.recv(1024).decode(errors="ignore").strip()
+
+        return banner
+
+    except:
+        return "No Banner"
 def scan_port(target_ip, port, scan_results):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(0.5)
-
     result = sock.connect_ex((target_ip, port))
-
     if result == 0:
         service = services.get(port, "Unknown Service")
+        banner = grab_banner(sock, port)
         with lock:
-            scan_results.append({"port": port, "service": service})
+            scan_results.append({
+                "port": port,
+                "service": service,
+                "banner": banner
+            })
             print(f"[OPEN] Port {port} ({service})")
+            if banner != "No Banner":
+                print(f"       Banner: {banner}")
         sock.close()
         return True
     sock.close()
@@ -46,19 +63,15 @@ def scan(target, start_port, end_port):
         scan_results = []
         # Scan every port in the specified range
         threads = []
-
         for port in range(start_port, end_port + 1):
             thread = threading.Thread(
                 target=scan_port,
                 args=(target_ip, port, scan_results)
             )
-
             threads.append(thread)
             thread.start()
-
         for thread in threads:
             thread.join()
-
         open_ports = len(scan_results)
         print("-" * 50)
         print("Scan Completed")
@@ -79,10 +92,12 @@ def scan(target, start_port, end_port):
     except KeyboardInterrupt:
         print("\nScan cancelled by user.")
         sys.exit() 
+parser = argparse.ArgumentParser(description="Python Network Port Scanner")
+
+parser.add_argument("target", help="Target IP address or domain")
+parser.add_argument("start_port", type=int, help="Starting port")
+parser.add_argument("end_port", type=int, help="Ending port")
+
+args = parser.parse_args()
 if __name__ == "__main__":
-    target = input("Enter Target IP or Domain: ")
-    start_port = int(input("Start Port: "))
-    end_port = int(input("End Port: "))
-    scan(target, start_port, end_port)
-
-
+    scan(args.target, args.start_port, args.end_port)
